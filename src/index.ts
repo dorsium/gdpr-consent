22f9b1ac3337400173e6ae1000d248a431928609
Grávuj Miklós Henrich <67593243+gravuj-miklos-henrich@users.noreply.github.com>
2025-07-08 16:10:36 +0300

    feat: export hook and badge

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { default as CookieConsent } from "./components/CookieConsent";
+export { useConsent } from "./hooks/useConsent";
+export { default as PoweredByDorsiumBadge } from "./components/PoweredByDorsiumBadge";
